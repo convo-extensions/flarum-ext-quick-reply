c3102368660f466e4be29a88ead60863bd8d10a7
Clark Winkelmann <clark.winkelmann@gmail.com>
2021-12-24 18:20:44 +0100

    Rename package, make text translatable, add icon

---
+++ js/src/forum/index.ts
@@ -0,0 +1,34 @@
+import app from 'flarum/forum/app';
+import {extend} from 'flarum/common/extend';
+import ItemList from 'flarum/common/utils/ItemList';
+import DiscussionPage from 'flarum/forum/components/DiscussionPage';
+import ReplyComposer from 'flarum/forum/components/ReplyComposer';
+
+app.initializers.add('quick-reply', () => {
+    // Automatically open composer when visiting a discussion page
+    // Flarum already takes care of closing unused composers when leaving the page
+    extend(DiscussionPage.prototype, 'show', function (returnValue: never, discussion: any) {
+        if (app.screen() === 'phone' && discussion.canReply() && !app.composer.composingReplyTo(discussion)) {
+            app.composer.load(ReplyComposer, {
+                user: app.session.user,
+                discussion,
+            });
+            app.composer.show();
+            app.composer.minimize();
+        }
+    });
+
+    // Change composer title to better reflect the fact it's a reply
+    extend(ReplyComposer.prototype, 'headerItems', function (this: ReplyComposer, items: ItemList) {
+        const {discussion} = this.attrs as any;
+
+        if (items.has('title')) {
+            // Change the text content of h3 > Link
+            items.get('title').children[2].children = [
+                app.translator.trans('quick-reply.forum.composer.title', {
+                    title: m('em', discussion.title()),
+                }),
+            ];
+        }
+    });
+});
